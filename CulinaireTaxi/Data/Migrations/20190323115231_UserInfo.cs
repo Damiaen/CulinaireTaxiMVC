3ec77656b5171f73cf13069f5f4e992ce83cb447
Damiaen <dtoussaint97@gmail.com>
2019-03-23 13:23:46 +0100

    UserInfo Migration

---
+++ CulinaireTaxi/Data/Migrations/20190323115231_UserInfo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CulinaireTaxi.Data.Migrations
+{
+    public partial class UserInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(maxLength: 450, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 256, nullable: true),
+                    Prefix = table.Column<string>(maxLength: 256, nullable: true),
+                    LastName = table.Column<string>(maxLength: 256, nullable: true),
+                    PostalCode = table.Column<string>(maxLength: 256, nullable: true),
+                    HouseNumber = table.Column<string>(maxLength: 256, nullable: true),
+                    HouseNumberPrefix = table.Column<string>(maxLength: 256, nullable: true),
+                    City = table.Column<string>(maxLength: 256, nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserInfo_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
