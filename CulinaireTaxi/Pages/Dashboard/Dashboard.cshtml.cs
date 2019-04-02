d79e52a9511de8ec8c481c851996810f646da158
Damiaen <dtoussaint97@gmail.com>
2019-04-02 14:28:47 +0200

    adminpanel authcheck

--- CulinaireTaxi/Pages/Dashboard/Dashboard.cshtml.cs
+++ CulinaireTaxi/Pages/Dashboard/Dashboard.cshtml.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CulinaireTaxi.Pages.Dashboard
 {
+    [Authorize]
     public class DashboardModel : PageModel
     {
         public void OnGet()
         {
 
         }
+
     }
 }
